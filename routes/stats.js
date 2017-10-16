2fb107fabba58a7bfa382fda8e487322f679fdb6
5aurabh <saurabnanda@gmail.com>
2017-10-16 13:47:53 +0530

    demography api done

--- routes/stats.js
+++ routes/stats.js
@@ -116,4 +116,56 @@ module.exports = function(settings){
 			return settings.serviceError(res);
 		})
 	})
+
+
+	function fetchEmployeesGroup(companyID, groupBy){
+		var query = "Select count(*) from AlumnusMaster group by DesignationId"
+		var queryArray = [companyID];
+		switch(groupBy){
+			case 'designation':
+				query = "Select count(*) as num, dm.Name from AlumnusMaster am inner join DesignationMaster dm on am.DesignationId=dm.designationId where am.CompanyId= ? group by am.DesignationId, dm.Name";
+				break;
+			case 'institute':
+				query = "Select count(*) as num, im.Name from AlumnusMaster am inner join EducationDetails ed on am.AlumnusId = ed.AlumnusId inner join InstituteMaster im on ed.InstituteId=im.InstituteId where am.CompanyId= ? group by ed.InstituteId, im.Name";
+				break;
+			case 'gender':
+				query = "Select count(*) as num, Sex as Name from AlumnusMaster group by Sex";
+				break;
+			case 'education':
+				query = "Select count(*) as num, cm.Name from AlumnusMaster am inner join EducationDetails ed on am.AlumnusId=ed.AlumnusId inner join CourseMaster cm on cm.CourseId=ed.CourseId where am.CompanyId= ? group by ed.CourseId, cm.Name";
+				break;
+			default:
+				break;
+		}
+		return settings.dbConnection().then(function(connection){
+			return settings.dbCall(connection, query, queryArray);
+		});		
+	}
+
+	app.get("/company/:companyID/demography", function(req, res){
+		var companyID = req.params.companyID;
+		var by = req.query.by || null;
+		if(!(by && ( by =='designation' || by=='institute' || by=='gender' || by=='education')))
+			return settings.unprocessableEntity(res);
+
+		var fetchingDemo = fetchEmployeesGroup(companyID, by);	
+		fetchingDemo.then(function(rows){
+			var data = [];
+			rows.forEach(function(aRow){
+				data.push({
+					name: aRow["Name"],
+					value: aRow["num"]
+				})
+			})
+			res.json({
+				status: "success",
+				data: data
+			});
+			return
+		})
+		.catch(function(err){
+			cprint(err,1);
+			return settings.serviceError(res);
+		})
+	})
 }
