f5237d903b0d11962df175b6900c99979ee6d122
5aurabh <saurabnanda@gmail.com>
2017-10-26 12:26:53 +0530

    enabled receive and sendmaessage sqs

---
+++ Q/sqs/q.js
@@ -0,0 +1,58 @@
+var aws = require('aws-sdk');
+var config = require('../../configuration.json')["q"];
+
+aws.config.update({
+	accessKeyId: config["accessKey"],
+    secretAccessKey: config["secretKey"],
+    region: "ap-south-1"
+})
+
+var sqs = new aws.SQS();
+
+var qURL ="https://sqs.ap-south-1.amazonaws.com/103293947098/test"
+
+var accountNumber = "103293947098";
+var params = {
+  QueueName: 'test', /* required */
+  QueueOwnerAWSAccountId: accountNumber
+};
+
+function sendMessage(){
+	var params = {
+        MessageBody: JSON.stringify({
+        	event: 'subscribe',
+        	id: 12334,
+        	email: 'testingabc@gmail.com',
+        	payload: {
+        		name: 'testing',
+        		company: 'Abc private limited'
+        	},
+        	templateId: 2,
+        	groupId: 1,
+        	dob: 'sometimestamp',
+        	timestamp: Date.now()
+        }),
+        QueueUrl: qURL,
+        DelaySeconds: 0
+    };
+    sqs.sendMessage(params, function(err, data){
+    	if(err)
+    		console.log(err)
+    	console.log(data)
+    })
+}	
+// sendMessage();
+
+function receiveMessage(){
+	var params = {
+		QueueUrl: qURL,
+		VisibilityTimeout: 60,
+		MaxNumberOfMessages: 10
+	}
+	sqs.receiveMessage(params, function(err, data){
+		console.log(data)
+	})
+}
+
+receiveMessage();
+
