9a09a294e7a120550885a931cd0e61d035d0a2cc
5aurabh <saurabnanda@gmail.com>
2017-11-15 15:16:51 +0530

    cleaning

--- routes/register/untitled.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(settings){
-	var app = settings.app;
-	var mode = settings.mode;
-	var config = settings.config;
-	var env = settings.env;
-	var cprint = settings.cprint;
-}
