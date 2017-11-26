ba793bfcc79f70da17049662b3e7b85f9a2ccfeb
5aurabh <saurabnanda@gmail.com>
2017-11-27 03:48:10 +0530

    dashboard changes, added news count

--- routes/dashboard/init.js
+++ routes/dashboard/init.js
@@ -1,4 +1,5 @@
 var moment = require("moment");
+var request = require('request')
 module.exports = function(settings){
 	var app = settings.app;
 	var mode = settings.mode;
@@ -9,14 +10,17 @@ module.exports = function(settings){
 	app.get("/company/:companyID/dashboard", function(req, res){
 		var companyID = req.params.companyID;
 		var currentMonth = 1+ moment().month();
-		var promiseArray = [ alumniCount(companyID), getBirthdayCount(companyID,currentMonth)]
+		var promiseArray = [ alumniCount(companyID), getBirthdayCount(companyID,currentMonth), getNewsCount(companyID)]
 		var initialPromise = Promise.all(promiseArray)
 		initialPromise.then(function(dataArray){
 			var alumniRows = dataArray[0];
 			var birthdayRows = dataArray[1];
+			var newsRows = (dataArray[2]) ? JSON.parse(dataArray[2]) : "";
 
 			var total = alumniRows[0]["totalCount"];
 			var birthdayCount = birthdayRows[0]["birthdayCount"];
+			var newsCount = (newsRows['data'])? newsRows['data'].length : 0
+
 			var data = [];
 			data.push({
 				id: "total",
@@ -25,6 +29,10 @@ module.exports = function(settings){
 			data.push({
 				id: "birthdayCount",
 				value: birthdayCount
+			})
+			data.push({
+				id: "totalNews",
+				value: newsCount
 			})
 			return res.json({
 				status: "success",
@@ -52,7 +60,22 @@ module.exports = function(settings){
 			return settings.dbCall(connection, query, queryArray);
 		});				
 	}
-	function getNewsCount(){
-			x
+	function getNewsCount(companyID){
+		return new Promise(function(fulfill, reject){
+			request.get({
+				url: config["services"]["news"]+"/group/"+companyID+"/news/"
+			}, function(err, response, body){
+				if(err){
+					cprint(err,1);
+					reject(err);
+				}
+				if(response.statusCode==200){
+					 fulfill(body);
+				}
+				else{
+					reject(body);
+				}
+			})
+		})
 	}
 }
